22bb5156d18a5b13d779d701da3acfb67c7e8d17
jpucket8 <jp@jpthedev.com>
2024-05-14 20:10:04 -0400

    implement error handling and async handling in server file

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import connectDB from "./config/db.js";
 import express from "express";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -12,13 +16,9 @@ app.get("/", (req, res) => {
 	res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-	res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
