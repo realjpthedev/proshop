24d298d564265eeb1b6bafc3ba1bdaef65593f48
jpucket8 <jp@jpthedev.com>
2024-07-03 20:04:58 -0400

    add line to return products in product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,6 +6,7 @@ import Product from "../models/productModel.js";
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
 	const products = await Product.find({});
+	res.json(products);
 });
 
 // @desc Fetch a product
