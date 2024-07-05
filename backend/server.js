8e43423d385388aa344fad0353ebcd92162c6f19
jpucket8 <jp@jpthedev.com>
2024-07-05 15:24:25 -0400

    add cookie parser and user routes to server file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import express from "express";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
@@ -12,11 +14,19 @@ connectDB();
 
 const app = express();
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
 	res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
