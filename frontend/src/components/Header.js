69040f7160d9a2160d10876e57ec65a879e90275
jpucket8 <jp@jpthedev.com>
2024-07-05 17:19:58 -0400

    implement logout functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container, Badge, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 
 import logo from "../assets/logo.png";
 
@@ -9,8 +12,19 @@ const Header = () => {
 	const { cartItems } = useSelector((state) => state.cart);
 	const { userInfo } = useSelector((state) => state.auth);
 
-	const logoutHandler = () => {
-		console.log("logout");
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const [logoutApiCall] = useLogoutMutation();
+
+	const logoutHandler = async () => {
+		try {
+			await logoutApiCall().unwrap();
+			dispatch(logout());
+			navigate("/login");
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	return (
