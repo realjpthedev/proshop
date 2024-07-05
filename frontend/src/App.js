4d2bb9ccaba206752d24358bd9d9a88ba610cc95
jpucket8 <jp@jpthedev.com>
2024-07-05 17:09:48 -0400

    add toast container to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 	return (
@@ -14,6 +17,7 @@ const App = () => {
 				</Container>
 			</main>
 			<Footer />
+			<ToastContainer />
 		</>
 	);
 };
