9cd289881e6cf70af9c3105944bf2a8af6212b79
jpucket8 <jp@jpthedev.com>
2024-07-06 13:48:43 -0400

    create checkout steps component

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import { saveShippingAddress } from "../slices/cartSlice";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
 	const cart = useSelector((state) => state.cart);
@@ -36,6 +37,7 @@ const ShippingScreen = () => {
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1>Shipping</h1>
 
 			<Form onSubmit={submitHandler}>
