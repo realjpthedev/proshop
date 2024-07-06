45232a2f8692a71e39e1e7a4df5f0f632e578479
jpucket8 <jp@jpthedev.com>
2024-07-06 13:30:03 -0400

    implement shipping screena and functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
 			<Route path="/cart" element={<CartScreen />} />
 			<Route path="/login" element={<LoginScreen />} />
 			<Route path="/register" element={<RegisterScreen />} />
+			<Route path="/shipping" element={<ShippingScreen />} />
 		</Route>
 	)
 );
