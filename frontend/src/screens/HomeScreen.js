ab22a7ad3d3a0669c05c8b35cb0e9a55cef222c6
jpucket8 <jp@jpthedev.com>
2024-07-08 17:26:58 -0400

    implement search feature

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
@@ -7,11 +7,19 @@ import Message from "../components/Message";
 import Paginate from "../components/Paginate";
 
 const HomeScreen = () => {
-	const { pageNumber } = useParams();
-	const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+	const { pageNumber, keyword } = useParams();
+	const { data, isLoading, error } = useGetProductsQuery({
+		keyword,
+		pageNumber,
+	});
 
 	return (
 		<>
+			{keyword && (
+				<Link to="/" className="btn btn-light mb-4">
+					Go Back
+				</Link>
+			)}
 			{isLoading ? (
 				<Loader />
 			) : error ? (
@@ -28,7 +36,11 @@ const HomeScreen = () => {
 							</Col>
 						))}
 					</Row>
-					<Paginate pages={data.pages} page={data.page} />
+					<Paginate
+						pages={data.pages}
+						page={data.page}
+						keyword={keyword ? keyword : ""}
+					/>
 				</>
 			)}
 		</>
