2e465091d7fc2d94e67d5cc247aa76e5efa4b26d
jpucket8 <jp@jpthedev.com>
2024-07-08 16:36:01 -0400

    implement pagination

--- frontend/src/screens/admin/ProductListScreen.js
+++ frontend/src/screens/admin/ProductListScreen.js
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaTimes, FaEdit, FaTrash } from "react-icons/fa";
@@ -10,9 +11,14 @@ import {
 } from "../../slices/productsApiSlice";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import Paginate from "../../components/Paginate";
 
 const ProductListScreen = () => {
-	const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+	const { pageNumber } = useParams();
+
+	const { data, isLoading, error, refetch } = useGetProductsQuery({
+		pageNumber,
+	});
 
 	const [createProduct, { isLoading: loadingCreateProduct }] =
 		useCreateProductMutation();
@@ -77,7 +83,7 @@ const ProductListScreen = () => {
 							</tr>
 						</thead>
 						<tbody>
-							{products.map((product) => (
+							{data.products.map((product) => (
 								<tr key={product._id}>
 									<td>{product._id}</td>
 									<td>{product.name}</td>
@@ -103,6 +109,8 @@ const ProductListScreen = () => {
 							))}
 						</tbody>
 					</Table>
+
+					<Paginate pages={data.pages} page={data.page} isAdmin={true} />
 				</>
 			)}
 		</>
